7b261bef291f53322c2358535ef926f54662cef4
Daria <lp900@yandex.ru>
2022-09-03 00:41:24 +0300

    Homework after the 9th seminar

---
+++ Example001/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 66: Задайте значения M и N. Напишите 
+// программу, которая найдёт сумму натуральных 
+// элементов в промежутке от M до N.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите первое число: ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int SumOfNumbers(int start, int end)
+{
+    if (start == end) return start;
+    if (start < end) return (start + SumOfNumbers(start + 1, end));
+    else return (start + SumOfNumbers(start - 1, end));
+}
+
+Console.WriteLine($"Сумма чисел в промежутке от {M} до {N} = {SumOfNumbers(M, N)}");
---
+++ Example002/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68: Напишите программу вычисления 
+// функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+
+Console.WriteLine("Введите первое число: ");
+int M = Convert.ToInt32(Console.ReadLine());  // start
+Console.WriteLine("Введите второе число: ");
+int N = Convert.ToInt32(Console.ReadLine());  // end
+
+if (M < 0 || N < 0)
+{
+    Console.WriteLine("Введите положительные числа!");
+    return;
+}
+
+int Ackermann(int start, int end)
+{
+    if (start == 0) return end + 1;
+    if (start != 0 && end == 0) return Ackermann(start - 1, 1);
+    if (start > 0 && end > 0) return Ackermann(start - 1, Ackermann(start, end - 1));
+    return Ackermann(start, end);
+}
+
+Console.WriteLine($"Результат вычисления функции Аккермана = {Ackermann(M, N)}");
